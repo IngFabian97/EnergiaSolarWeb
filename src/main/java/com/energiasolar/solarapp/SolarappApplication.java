6d36933ee955b058d7b099247fb65a428b07b56d
IngFabian97 <fabian.guarin@hotmail.com>
2025-02-06 18:40:19 -0500

    Inicilizacion del proyecto

---
+++ src/main/java/com/energiasolar/solarapp/SolarappApplication.java
@@ -0,0 +1,13 @@
+package com.energiasolar.solarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/energiasolar/solarapp/SolarappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energiasolar.solarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
